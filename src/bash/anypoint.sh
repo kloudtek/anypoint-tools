a437dd0eda886c118f14f8e04d8951a6037a1745
Yannick Menager <ymenager@kloudtek.com>
2017-09-18 19:42:19 -0700

    In progress

--- src/bash/anypoint.sh
+++ src/bash/anypoint.sh
@@ -29,21 +29,18 @@ authenticate() {
 }
 
 getOrgId() {
-    echo "Retrieving id of organization ${1}"
     JSON=$(curl -s -H "${AUTH}" "https://anypoint.mulesoft.com/accounts/api/me")
     ORG_ID=$(echo ${JSON} | jq -r ".user.memberOfOrganizations[] | select ( .name == \"${1}\" ).id")
         failIfUnset "Failed to retrieve org id ${1}" "${ORG_ID}" "${JSON}"
 }
 
 getEnvId() {
-    echo "Retrieving id of environment ${2} from org id ${1}"
     JSON=$(curl -s -H "${AUTH}" "https://anypoint.mulesoft.com/accounts/api/organizations/${1}/environments")
     ENV_ID=$(echo ${JSON} | jq -r ".data[] | select ( .name == \"${2}\" ).id")
     failIfUnset "Failed to retrieve org id ${2} from org id ${1}" "${ENV_ID}" "${JSON}"
 }
 
 getApiId() {
-    echo "Retrieving API id"
     JSON=$(curl -s -H "${AUTH}" "https://anypoint.mulesoft.com/apiplatform/repository/v2/organizations/${ORG_ID}/apis?ascending=false&limit=200&query=${1}")
     API_ID=$(echo ${JSON} | jq ".apis[] | select(.name == \"${1}\").versions[] | select(.name == \"${2}\").id")
 }
@@ -65,14 +62,45 @@ getServerId() {
     SERVER_ID=$(echo ${JSON} | jq ".data[0] | select ( .name == \"$3\" ).id ")
 }
 
+deleteServer() {
+    curl -s -X DELETE -H "${AUTH}" -H "X-ANYPNT-ORG-ID:${1}" -H "X-ANYPNT-ENV-ID:${2}" https://anypoint.mulesoft.com/hybrid/api/v1/servers/${3}
+}
+
 createServerGroup() {
+    SERVER_GROUP=$(curl -s -X POST -d "{\"name\":\"${3}\",\"serverIds\":[$4]}" -H "Content-Type: application/json" -H "${AUTH}" -H "X-ANYPNT-ORG-ID:${1}" -H "X-ANYPNT-ENV-ID:${2}" https://anypoint.mulesoft.com/hybrid/api/v1/serverGroups | jq .data.id)
+}
+
+listServersInGroup() {
+    listServers $1 $2
+    SERVER_IDS=($(echo ${JSON} | jq ".data[0] | select ( .id == $3 ).details.servers[].id"))
+}
+
+deleteServerGroup() {
+    curl -s -X DELETE -H "${AUTH}" -H "X-ANYPNT-ORG-ID:${1}" -H "X-ANYPNT-ENV-ID:${2}" https://anypoint.mulesoft.com/hybrid/api/v1/serverGroups/${3}
+    echo deleted server $3 from org $1 env $2
+}
+
+listApps() {
+    curl -s -H "${AUTH}" -H "Content-Type: application/json" -H "X-ANYPNT-ORG-ID:${1}" -H "X-ANYPNT-ENV-ID:${2}" https://anypoint.mulesoft.com/hybrid/api/v1/applications?targetId=${3} | jq .
 }
 
 authenticate
 getOrgId "Integration Group"
-getEnvId ${ORG_ID} "Dev"
-getServerId ${ORG_ID} ${ENV_ID} "DevGroup-1"
-echo "Server Id = ${SERVER_ID}"
+getEnvId ${ORG_ID} "tst"
+getServerId ${ORG_ID} ${ENV_ID} "tstGroup"
+echo ${SERVER_ID}
+listApps ${ORG_ID} ${ENV_ID} ${SERVER_ID}
+
+# create server group
+#getServerId ${ORG_ID} ${ENV_ID} "tst"
+#createServerGroup ${ORG_ID} ${ENV_ID} "testgroup" ${SERVER_ID}
+
+# delete server group
+#getServerId ${ORG_ID} ${ENV_ID} "testgroup"
+#listServersInGroup ${ORG_ID} ${ENV_ID} ${SERVER_ID}
+#deleteServerGroup  ${ORG_ID} ${ENV_ID} ${SERVER_ID}
+
+
 #getServerRegistrationKey ${ORG_ID} ${ENV_ID}
 #echo "Registration key=${ARM_REG_KEY}"
 #createEnv ${ORG_ID} "delmenow" "sandbox"
