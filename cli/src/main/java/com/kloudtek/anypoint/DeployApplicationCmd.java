f9b149eae8f02ab1a6354d44536a844d4da18c8b
Yannick Menager <ymenager@kloudtek.com>
2018-02-01 00:04:38 -0800

    Added parallel provisioning / deployment

--- cli/src/main/java/com/kloudtek/anypoint/DeployApplicationCmd.java
+++ cli/src/main/java/com/kloudtek/anypoint/DeployApplicationCmd.java
@@ -6,8 +6,8 @@ import com.kloudtek.anypoint.runtime.ApplicationDeploymentFailedException;
 import com.kloudtek.anypoint.runtime.Server;
 import com.kloudtek.util.TempFile;
 import com.kloudtek.util.UnexpectedException;
-import com.kloudtek.util.UserDisplayable;
 import com.kloudtek.util.UserDisplayableException;
+import com.kloudtek.util.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine.Command;
@@ -19,6 +19,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Command(name = "deploy", description = "Deploy Application", sortOptions = false)
 public class DeployApplicationCmd extends AbstractEnvironmentCmd {
@@ -48,81 +52,97 @@ public class DeployApplicationCmd extends AbstractEnvironmentCmd {
 
     @Override
     protected void execute(Environment environment) throws Exception {
+        ExecutorService deployThreadPool = Executors.newCachedThreadPool();
         Server server = environment.findServer(target);
-        HashMap<String,Application> deployed = new HashMap<>();
+        HashMap<String, Application> deployed = new HashMap<>();
         for (File appArchive : appArchives) {
-            if( ! appArchive.exists() ) {
-                logger.error("Application file not found: "+appArchive.getPath());
+            if (!appArchive.exists()) {
+                logger.error("Application file not found: " + appArchive.getPath());
             }
         }
         int appCount = appArchives.size();
-        if( appNames.size() != appCount) {
+        if (appNames.size() != appCount) {
             throw new UserDisplayableException("Number of names do not match number of files specified");
         }
         ArrayList<String> failed = new ArrayList<>(deployed.size());
-        for(int i = 0; i< appCount; i++) {
-            File appArch = appArchives.get(i);
-            String appName = appNames.get(i);
-            try {
-                if (appArch.exists()) {
-                    if (!skipProvisioning) {
-                        ProvisioningConfig provisioningConfig = new ProvisioningConfig(provisioningParams, extraAccess);
-                        if (legacyMode) {
-                            provisioningConfig.setLegacyMode(true);
-                            provisioningConfig.setDescriptorLocation("classes/anypoint.json");
+        ArrayList<Future> tasks = new ArrayList<>();
+        for (int i = 0; i < appCount; i++) {
+            final int idx = i;
+            tasks.add(deployThreadPool.submit(() -> {
+                File appArch = appArchives.get(idx);
+                String appName = appNames.get(idx);
+                try {
+                    if (appArch.exists()) {
+                        if (!skipProvisioning) {
+                            ProvisioningConfig provisioningConfig = new ProvisioningConfig(provisioningParams, extraAccess);
+                            if (legacyMode) {
+                                provisioningConfig.setLegacyMode(true);
+                                provisioningConfig.setDescriptorLocation("classes/anypoint.json");
+                            }
+                            logger.info("Provisioning: " + appName);
+                            TransformList transformList = parent.getClient().provision(server.getParent().getParent(), appArch, provisioningConfig, envSuffix);
+                            if (transformList != null) {
+                                try {
+                                    appArch = transformList.applyTransforms(appArch, null);
+                                } catch (Exception e) {
+                                    throw new UserDisplayableException("An error occured while applying application " + appName + " transformations: " + e.getMessage(), e);
+                                }
+                            }
+                            logger.info("Provisioning completed: " + appName);
                         }
-                        logger.info("Provisioning: " + appName);
-                        TransformList transformList = parent.getClient().provision(server.getParent().getParent(), appArch, provisioningConfig, envSuffix);
-                        if (transformList != null) {
-                            try {
-                                appArch = transformList.applyTransforms(appArch, null);
-                            } catch (Exception e) {
-                                throw new UserDisplayableException("An error occured while applying application "+appName+" transformations: " + e.getMessage(), e);
+                        if (!force) {
+                            if (server.checkApplicationExist(appName, appArch, true)) {
+                                logger.info("Application already deployed: " + appName);
+                                return;
                             }
                         }
-                        logger.info("Provisioning completed: "+appName);
-                    }
-                    if (!force) {
-                        if (server.checkApplicationExist(appName, appArch, true)) {
-                            logger.info("Application already deployed: "+appName);
-                            continue;
+                        logger.info("Deploying application: " + appName);
+                        try {
+                            Application application = server.deploy(appName, appArch);
+                            deployed.put(appArch.getName(), application);
+                        } catch (IOException e) {
+                            throw new UserDisplayableException("Error loading application " + appName + " : " + e.getMessage(), e);
                         }
+                    } else {
+                        throw new UserDisplayableException("File not found: " + appArch.getPath());
                     }
-                    logger.info("Deploying application: "+appName);
-                    try {
-                        deployed.put(appArch.getName(),server.deploy(appName, appArch));
-                    } catch (IOException e) {
-                        throw new UserDisplayableException("Error loading application " + appName+" : "+e.getMessage(), e);
+                } catch (Exception e) {
+                    logger.error("Failed to deploy " + appName + " : " + e.getMessage(), e);
+                    failed.add(appName);
+                } finally {
+                    if (appArchives instanceof TempFile) {
+                        IOUtils.close((TempFile) appArch);
                     }
-                } else {
-                    throw new UserDisplayableException("File not found: " + appArch.getPath());
                 }
-            } catch (Exception e) {
-                logger.error("Failed to deploy "+appName+" : "+e.getMessage(),e);
-                failed.add(appName);
-            } finally {
-                if (appArchives instanceof TempFile) {
-                    ((TempFile) appArchives).close();
-                }
-            }
+            }));
         }
+        for (Future task : tasks) {
+            task.get();
+        }
+        tasks.clear();
         logger.info("Applications deployment completed");
         if (!skipWait) {
             logger.info("Waiting for apps to start");
             for (Map.Entry<String, Application> entry : deployed.entrySet()) {
-                try {
-                    entry.getValue().waitDeployed();
-                } catch (ApplicationDeploymentFailedException e) {
-                    logger.error("Failed to deploy "+entry.getKey()+" : "+e.getMessage(),e);
-                    failed.add(entry.getKey());
-                }
+                tasks.add(deployThreadPool.submit(() -> {
+                    try {
+                        entry.getValue().waitDeployed();
+                    } catch (ApplicationDeploymentFailedException e) {
+                        logger.error("Failed to deploy " + entry.getKey() + " : " + e.getMessage(), e);
+                        failed.add(entry.getKey());
+                    }
+                    return null;
+                }));
+            }
+            for (Future task : tasks) {
+                task.get();
             }
         }
         int failedCount = failed.size();
-        if( failedCount == 0 ) {
+        if (failedCount == 0) {
             logger.info("All apps successfully started");
         } else {
-            throw new UnexpectedException((appCount-failedCount)+" out of "+appCount+" apps started successfully. Failed apps: "+failed);
+            throw new UnexpectedException((appCount - failedCount) + " out of " + appCount + " apps started successfully. Failed apps: " + failed);
         }
     }
 }
