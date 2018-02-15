cd8c2ade93206228c6f7e1472b6cdd1d8bbc4097
Yannick Menager <ymenager@kloudtek.com>
2018-02-15 10:35:41 -0800

    Small fix in anypoint.sh location

--- cli/src/main/assembly/anypoint.sh
+++ cli/src/main/assembly/anypoint.sh
@@ -8,4 +8,4 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-java -jar ${DIR}/anypoint.jar $@
+java -jar ${DIR}/../lib/anypoint.jar $@
