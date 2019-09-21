67d90c7393d93fe4d66aaa6b7583057eb661ef9b
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2019-09-20 18:14:26 -0700

    Workaround limited access accounts

--- lib/src/main/java/com/kloudtek/anypoint/api/APIContractList.java
+++ lib/src/main/java/com/kloudtek/anypoint/api/APIContractList.java
@@ -10,24 +10,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class APIContractList extends PaginatedList<APIContract, API> {
-    private String orgId;
-    private String envId;
-    private String apiId;
-
     public APIContractList(API api) throws HttpException {
         super(api);
-        envId = api.getParent().getParent().getId();
-        envId = api.getParent().getId();
-        apiId = api.getId();
-        limit = 50;
-        download();
-    }
-
-    public APIContractList(API parent, String orgId, String envId, String apiId) throws HttpException {
-        super(parent);
-        this.orgId = orgId;
-        this.envId = envId;
-        this.apiId = apiId;
         limit = 50;
         download();
     }
@@ -35,7 +19,7 @@ public class APIContractList extends PaginatedList<APIContract, API> {
     @NotNull
     @Override
     protected URLBuilder buildUrl() {
-        URLBuilder url = new URLBuilder("/apimanager/api/v1/organizations/" + orgId + "/environments/" + envId + "/apis/" + apiId + "/contracts")
+        URLBuilder url = new URLBuilder("/apimanager/api/v1/organizations/" + parent.getParent().getParent().getId() + "/environments/" + parent.getParent().getId() + "/apis/" + parent.getId() + "/contracts")
                 .param("ascending", "true");
         return url;
     }
